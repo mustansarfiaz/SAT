f06d6fc5c3062efe5da58d43f34253d65b5d7e24
Mustansar Fiaz <mustansar3797@gmail.com>
2023-08-08 23:47:53 +0400

    Update eval_func.py

--- eval_func.py
+++ eval_func.py
@@ -1,6 +1,4 @@
-# This file is part of COAT, and is distributed under the
-# OSI-approved BSD 3-Clause License. See top-level LICENSE file or
-# https://github.com/Kitware/COAT/blob/master/LICENSE for details.
+
 
 import os.path as osp
 import numpy as np
