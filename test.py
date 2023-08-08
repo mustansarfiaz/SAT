7d6d75d0fb03391c695c324bc714f535f6e370e4
Mustansar Fiaz <mustansar3797@gmail.com>
2023-08-08 23:47:38 +0400

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,3 @@
-# This file is part of COAT, and is distributed under the
-# OSI-approved BSD 3-Clause License. See top-level LICENSE file or
-# https://github.com/Kitware/COAT/blob/master/LICENSE for details.
 
 import argparse
 import datetime
